146ca9c7c51ff0461dfaa3136a34db903069d7c3
Mihael Bun <bunmihail@gmail.com>
2022-07-18 00:19:36 +0300

    Example 10 complite

---
+++ Example010_ArrayMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 23, 31, 34, 80, 52, 46, 74, 80 };
+int n = array.Length;
+int find = 80;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
